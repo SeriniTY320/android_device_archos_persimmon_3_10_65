a13b23218fc7ba3fd8023d0b08551edc0629710e
Yuri <SeriniTY320@gmail.com>
2017-01-12 15:24:17 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,13 +2,13 @@
 #$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := 50 Cobalt
+PRODUCT_RELEASE_NAME := Prime L
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/archos/persimmon/device_persimmon.mk)
+$(call inherit-product, device/highscreen/prime_l/device_persimmon.mk)
 
 # Configure dalvik heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
@@ -17,12 +17,11 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := persimmon
-PRODUCT_NAME := lineage_persimmon
-PRODUCT_BRAND := Archos
-PRODUCT_MODEL := Archos 50 Cobalt
-PRODUCT_MANUFACTURER := Archos
-
+PRODUCT_DEVICE := prime_l
+PRODUCT_NAME := lineage_prime_l
+PRODUCT_BRAND := Highscreen 
+PRODUCT_MODEL := Prime L
+PRODUCT_MANUFACTURER := Highscreen
 PRODUCT_DEFAULT_LANGUAGE := ru
 PRODUCT_DEFAULT_REGION   := RU
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
